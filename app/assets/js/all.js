4e91f2d38504bdf9ed5934c57e2d904a079fa43d
LokiH <larry0708@hotmail.com>
2021-08-26 00:21:14 +0800

    index.html completed

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,26 @@
-$(function() {
+
+$(function () {
   console.log('Hello Bootstrap5');
+});
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 30,
+    nav: false,
+    dots: false,
+    slideTransition: 'linear',
+    responsive: {
+      768: {
+        items: 2.2
+      },
+      1000: {
+        items: 1.2
+      }
+    }
+  });
+
+
+  $('.js-scrollLeft').scrollLeft(1500);
 });
